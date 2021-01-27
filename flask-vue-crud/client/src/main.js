527c8af85dbe4cb097d7f3bc1432e9267bf88d66
Anderson Martins <anderson77martins@outlook.com>
2021-01-27 17:40:08 -0300

    Refazendo serve client 'This relative relative was not found'

--- flask-vue-crud/client/src/main.js
+++ flask-vue-crud/client/src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app');
